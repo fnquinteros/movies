ab1036a69cd830a53bb432d32e4f9e12f888e7d0
Felipe Quinteros <felipe@nomady.camp>
2023-02-07 15:01:45 +0100

    base state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState<any[]>([]);
+
+  const getMovies = async () => {
+    const data = await axios
+      .get("https://63e21881109336b6cbff6aa1.mockapi.io/api/Movies")
+      .then((response) => response.data)
+      .catch((err) => console.log(err));
+    return data;
+  };
+
+  useEffect(() => {
+    getMovies().then((data) => setMovies(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movies!</h1>
+      <div style={{ display: "flex" }}>
+        {movies.map((m) => {
+          return (
+            <div key={m.name}>
+              <h2>{m.name}</h2>
+              <p>release date: {m.release_date}</p>
+              <p>category: {m.category}</p>
+              <img src={m.img} style={{ width: "50%" }} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
